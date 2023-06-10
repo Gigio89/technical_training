124c547176f0d626f4946dae793a0fe6865ae2d7
Gigio89 <it@infinion.ch>
2023-06-10 12:16:01 +0000

    [ADD] New Module

---
+++ academy/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Odoo Academy',
+    'summary': """Modile to handle Course and Sessions""",
+    'description': """Module to handle:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    'license': 'OPL-1',
+    'author': 'Gigio89',
+    'website': 'www.infinion.ch',
+    'category': 'Custom Modules/Tech Training',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [],
+    'demo': [
+    'demo/academy_demo.xml',
+    ],
+    'application': True,
+}
