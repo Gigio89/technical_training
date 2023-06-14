2b191a03ac8cf9001ce90b70c5b738e9f0f02ad6
Gigio89 <it@infinion.ch>
2023-06-14 13:06:28 +0000

    [ADD] New Module

---
+++ spaceship/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Spaceship Mission',
+    'summary': """Odoo Space Mission""",
+    'description': """Module to track different metrics of spaceship""",
+    'license': 'OPL-1',
+    'author': 'Gigio89',
+    'website': 'www.infinion.ch',
+    'category': 'Custom Modules/Tech Training',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+'application': True,
+}
