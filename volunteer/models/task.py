ccf3c7b627e339c5ad35d08acf350a02634600cc
Gigio89 <it@infinion.ch>
2023-06-15 12:56:41 +0000

    [ADD] Demo

--- volunteer/models/task.py
+++ volunteer/models/task.py
@@ -5,6 +5,19 @@ class Task(models.Model):
     _description = "Odoo Technical Training"
 
     name = fields.Char(string="Title", required=True)
-    active = fields.Boolean(string="Active", default=True)
+    
+    start_time = fields.Datetime(string="Start Time")
+    stop_time = fields.Datetime(string="Stop Time")
+    occurences = fields.Integer(string="Occurences")
+    description = fields.Text(string="Description")
+    task_type = fields.Selection(string="Task Type",
+                                 selection=[
+                                     ('to_do','To Do'),
+                                     ('project','Project'),
+                                     ('meeting','Meeting'),
+                                 ],)
+    
+
+    
     
 
