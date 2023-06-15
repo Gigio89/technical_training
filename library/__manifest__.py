50ad2b0ffbcf44a098a976b903605043a6b56345
Gigio89 <it@infinion.ch>
2023-06-15 07:58:55 +0000

    [ADD] New Module

---
+++ library/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Library',
+    'summary': """Exercise Module 2""",
+    'description': """Day 2 Exercise""",
+    'license': 'OPL-1',
+    'author': 'Gigio89',
+    'website': 'www.infinion.ch',
+    'category': 'Custom Modules',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
